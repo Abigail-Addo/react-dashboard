a0dc639d79bf18e0cbabf728fac20a3a1f1fd428
abigail <abigailadwoaaddo@gmail.com>
2024-08-14 15:30:19 +0000

    Update

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,30 +1,31 @@
 // import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Dashboard from './Pages/Home';
-import Customers from './Pages/Customers';
-import Orders from './Pages/Orders';
-import Products from './Pages/Products';
-import Login from './Pages/Login';
-import { ProtectedRoute } from './ProtectedRoute';
+import Dashboard from "./Pages/Home";
+import Customers from "./Pages/Customers";
+import Orders from "./Pages/Orders";
+import Products from "./Pages/Products";
+import Login from "./Pages/Login";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 const Router = () => {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/admin/login" element={<Login />} />
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin/login" replace />} />
+          <Route path="/admin/login" element={<Login />} />
 
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/customers" element={<Customers />} />
-                        <Route path="/orders" element={<Orders />} />
-                        <Route path="/products" element={<Products />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
-}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/products" element={<Products />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default Router
+export default Router;
